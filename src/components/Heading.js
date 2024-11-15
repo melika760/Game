0c9e52aaec362d5ec67e377da20fe884fe9ad909
melika760 <melikaaatafazoli@gmail.com>
2024-11-15 14:19:16 +0000

    add more animation

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -1,16 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import logo from "../images/logo.svg"
 import styles from "./Heading.module.css"
 import { useScore } from './context/ScoreContext'
 const Heading = () => {
   const {score}=useScore()
+  const[animate,setanimate]=useState(false)
+  useEffect(()=>{
+setanimate(true)
+const timeout=setTimeout(()=>setanimate(false),500)
+return()=>clearTimeout(timeout)
+  },[score])
   return (
   <header className={styles.heading}>
    
 <img src={logo} alt='logo' className={styles.Img}/>
 <div className={styles.contents}>
 <p>Score</p>
-<h2>{score}</h2>
+<h2 className={animate && styles.animate}>{score}</h2>
    
 </div>
   </header>
